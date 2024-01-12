29fabc265a790c5a9f886ab586ee93511f68e13a
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2024-01-12 12:34:02 +0530

    C++ Graphics

--- Scaling.cpp
+++ Scaling.cpp
@@ -7,19 +7,19 @@ int main()
 {
 	cout<<"\n******************* 2D Scaling  ***************************\n";
 	int gd=DETECT,gm;
-	int x1,x2,y1,y2,s;
+	int x1,x2,y1,y2,sx,sy;
 	cout<<"\nEnter the 2 line end points : ";
 	cin>>x1>>y1>>x2>>y2;
 	cout<<"\nNow enter the Scaling number : ";
-	cin>>s;
+	cin>>sx>>sy;
 	initgraph(&gd,&gm,"C:\\tc\\bgi");
 	outtextxy(10,10,"********************** 2D Scaling  ***************************");
 	outtextxy(x1,y1,"Line before Scaling");
 	line(x1,y1,x2,y2);
-	x1-=s;
-	y1-=s;
-	x2+=s;
-	y2+=s;
+	x1*=sx;
+	y1*=sy;
+	x2*=sx;
+	y2*=sy;
 	outtextxy(x1,y1,"Line after Scaling");
 	line(x1,y1,x2,y2);
 	getch();
